3245ad2ab05bcc2af0933558b78f263cc0ae445f
rnsabiyera <97302389+rnsabiyera@users.noreply.github.com>
2026-09-11 12:56:22 +0100

    feat: given two numbers perform subtraction

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,8 +4,15 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine($"The total is {Add(2,4)}");
 
+Console.WriteLine($"The total is {Subtract(2,4)}");
+
 
 int  Add(int a, int b)
 {
     return 6 + 9;
+}
+
+int Subtract(int a, int b)
+{
+    return a - b;
 }
